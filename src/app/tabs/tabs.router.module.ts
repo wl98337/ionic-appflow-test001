474f4e9dea2872a1f5ab6b9114445ed7ce9ec8fb
Li, Wenya Colin [GCB-OT] <wl98336@imcap.ap.ssmb.com>
2019-05-28 19:18:46 +0800

    add contactus function

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -35,6 +35,15 @@ const routes: Routes = [
         ]
       },
       {
+        path: 'contactus',
+        children: [
+          {
+            path: '',
+            loadChildren: '../service/contactus/contactus.module#ContactusPageModule'
+          }
+        ]
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'

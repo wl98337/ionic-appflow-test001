2281a513cc54d6a08a77fd7bc0ed03cdb3bb2a27
Li, Wenya Colin [GCB-OT] <wl98336@imcap.ap.ssmb.com>
2019-05-28 19:22:39 +0800

    add contactus function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'contactus', loadChildren: './service/contactus/contactus.module#ContactusPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
 ];
 @NgModule({
   imports: [
